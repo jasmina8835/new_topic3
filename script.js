8cba67e55a4c768f153c576c49788e3d95a24cb0
jasmina8835 <sunnatovajasmina8002@gmail.com>
2024-02-16 19:55:33 +0500

    topicss

---
+++ script.js
@@ -0,0 +1,56 @@
+// let headh1 = document.querySelector(".head");
+// let inp = document.querySelector("input");
+
+
+// console.log(inp);
+
+
+
+// headh1.addEventListener("click", clickFunc);
+// headh1.addEventListener("mouseover", clickFunc);
+// headh1.addEventListener("dblclick", clickFunc);
+// inp.addEventListener("focus", clickFunc);
+// inp.addEventListener("keydown", clickFunc);
+// inp.addEventListener("keyup", clickFunc);
+
+
+
+// function clickFunc() {
+//     console.log("tugma bosildi");
+// }
+
+
+
+// let containerJs = document.querySelector('.container')
+// let name = "Zarrux";
+// let age = "25";
+
+
+// let result = `my name is ${name} , I am ${age} years old`
+
+
+
+
+
+
+
+
+
+
+
+// containerJs.innerHTML = `
+//     <h1 class= "head">Hello Template</h1>
+//     <h2 style= "color: red; font-size: 40px">${result}</h2>
+// `
+
+
+containerJs.innerHTML = `
+       <h1 class="head1">Contact Form ${result}</h1>
+       <h3 class="head3">Please till all exercises ${result}</h3>
+       <hr>
+       <h4 class= "head4">name</h4>
+       <h4 class= "head4">email</h4>
+       <h4 class= "gender">gender</h4>
+       <h4 class= "message">message</h4>
+       <button>Send</button>
+`
